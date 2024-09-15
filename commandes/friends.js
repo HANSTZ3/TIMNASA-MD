cbe3735d59899fdd330a693a5d74c4e6a42bc75d
HANSTZ3/HANS-MD <mpyabottzhans@gmail.com>
2024-09-15 14:09:26 -0700

    Add files via upload

---
+++ commandes/friends.js
@@ -0,0 +1,68 @@
+const {
+  zokou
+} = require("../framework/zokou");
+const {
+  getAllSudoNumbers,
+  isSudoTableNotEmpty
+} = require("../bdd/sudo");
+const conf = require("../set");
+zokou({
+  'nomCom': "agents",
+  'categorie': 'General',
+  'reaction': "🌛"
+}, async (_0x1b06c5, _0x54bb8b, _0x2358bf) => {
+  const {
+    ms: _0x2aecc0,
+    mybotpic: _0x43a6e2
+  } = _0x2358bf;
+  const _0x21b56d = [{
+    'nom': "TIMNASA from Tanzania",
+    'nom': "HANS Tech from Tanzania 🇹🇿",
+    'numero': "load..."
+  }, {
+    'nom': "TIMNASA from TANZANIA ",
+    'numero': "255784766591"
+  }, {
+    'nom': "HANSTZ",
+    'numero': "255756530143"
+  }];
+  let _0x2d5c7e = "Hello👋  *I'm TIMNASA Md Wa Bot* \nThe Following Numbers Are For  *TIMENASA_MD* Agents, \nYou Can Ask Them Anything Regarding yesser Bot \nFollow Our Channel For More Tech :https://wa.me/message/J2ZL2GNK4GIUA1 \n*KEEP USING TIMNASA MD*:\n\n";
+  for (const _0x14eeec of _0x21b56d) {
+    _0x2d5c7e += "----------------\n(●) " + _0x14eeec.nom + " : https://wa.me/" + _0x14eeec.numero + "\n";
+  }
+  var _0x11d31d = _0x43a6e2();
+  if (_0x11d31d.match(/\.(mp4|gif)$/i)) {
+    try {
+      _0x54bb8b.sendMessage(_0x1b06c5, {
+        'video': {
+          'url': _0x11d31d
+        },
+        'caption': _0x2d5c7e
+      }, {
+        'quoted': _0x2aecc0
+      });
+    } catch (_0x55af9c) {
+      console.log("🥵🥵 Menu erreur " + _0x55af9c);
+      repondre("🥵🥵 Menu erreur " + _0x55af9c);
+    }
+  } else {
+    if (_0x11d31d.match(/\.(jpeg|png|jpg)$/i)) {
+      try {
+        _0x54bb8b.sendMessage(_0x1b06c5, {
+          'image': {
+            'url': _0x11d31d
+          },
+          'caption': _0x2d5c7e
+        }, {
+          'quoted': _0x2aecc0
+        });
+      } catch (_0x39b1ed) {
+        console.log("🥵🥵 Menu erreur " + _0x39b1ed);
+        repondre("🥵🥵 Menu erreur " + _0x39b1ed);
+      }
+    } else {
+      repondre(_0x11d31d);
+      repondre("link error");
+    }
+  }
+});
---
+++ commandes/reboot.js
@@ -0,0 +1,31 @@
+﻿const {zokou}=require("../framework/zokou")
+
+
+
+
+
+
+
+zokou({nomCom:"restart",categorie:"Mods",reaction:"☔"},async(dest,z,com)=>{
+
+
+  
+const{repondre,ms,dev,superUser}=com;
+
+  if(!superUser)
+  {
+    return repondre("This command is for owner or TIMNASA TEACH");
+  }
+
+  const {exec}=require("child_process")
+
+    repondre("TIMNASA-MD  bot Restarting ⏳");
+
+  exec("pm2 restart all");
+  
+
+  
+
+
+
+})
